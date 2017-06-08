3c58d766a77dd9e06be9d8b9c283cb70a994f579
mseeger <mseeger@mseeger-ltm.sfdcgomobile.salesforce.com>
2017-06-07 21:00:36 -0500

    Added absolute links to Stylesheets

--- views/header.php
+++ views/header.php
@@ -4,8 +4,8 @@
 <title>Cloudcrest Capital</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="./w3.css">
-<link rel="stylesheet" href="./cloudcrest.css">
+<link rel="stylesheet" href="/public/css/w3.css">
+<link rel="stylesheet" href="public/css/w3-ext.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
