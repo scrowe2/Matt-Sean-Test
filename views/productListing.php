ae03625590b4133586ace7230508b1ccae96b438
mseeger <mseeger@mseeger-ltm.sfdcgomobile.salesforce.com>
2017-06-14 17:13:13 -0500

    Removed w3-image class from Product Listing

--- views/productListing.php
+++ views/productListing.php
@@ -14,7 +14,7 @@ foreach($context->result->records as $prod){
 ?>
     <tr class="w3-row w3-hide-small">
         <td class="w3-cell">
-            <img style="height: 80px; width: auto;" class="w3-image" src="https://mfgqtc.my.salesforce.com/servlet/servlet.ImageServer?id=<?=$prod->SBQQ__ProductPictureID__c ?>&oid=00D460000000MPU" />
+            <img style="height: 80px; width: auto;" src="https://mfgqtc.my.salesforce.com/servlet/servlet.ImageServer?id=<?=$prod->SBQQ__ProductPictureID__c ?>&oid=00D460000000MPU" />
         </td>
             
         <td class="w3-cell"><?=$prod->Name ?></td>
@@ -36,7 +36,7 @@ foreach($context->result->records as $prod){
             <div class="w3-container w3-center">
                 <h3><?=$prod->Name ?></h3>
                 <p><?=$prod->ProductCode ?></p>
-                <img style="height: 160px; width: auto;" class="w3-image" src="https://mfgqtc.my.salesforce.com/servlet/servlet.ImageServer?id=<?=$prod->SBQQ__ProductPictureID__c ?>&oid=00D460000000MPU" />
+                <img style="height: 160px; width: auto;" src="https://mfgqtc.my.salesforce.com/servlet/servlet.ImageServer?id=<?=$prod->SBQQ__ProductPictureID__c ?>&oid=00D460000000MPU" />
                 <p><?=$prod->Description ?></p>
                 <?php if($prod->Configurable_on_Website__c){ ?>
                 <a href="/products/configure?pid=<?= $prod->Id ?>" class="w3-button w3-red w3-text-black w3-hover-black w3-hover-text-red">Build It</a>
