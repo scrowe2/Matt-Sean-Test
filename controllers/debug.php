92f7a441ac6c5989bef75014e9a5aeb2f13c462a
mseeger <mseeger@mseeger-ltm.sfdcgomobile.salesforce.com>
2017-06-14 15:30:12 -0500

    Trying to test still

--- controllers/debug.php
+++ controllers/debug.php
@@ -14,10 +14,7 @@ class debug extends Controller{
     public function Index(){
         
         $sfdc = new SFDCConnector();
-        $prods = $sfdc->query("SELECT Id, sbqq__ProductPictureID__c, Name, ProductCode FROM Product2 LIMIT 30");
-        foreach ($prods->result->records as $prod){
-            var_dump($prod);
-        }
+        var_dump($sfdc);
     }
 }
 
