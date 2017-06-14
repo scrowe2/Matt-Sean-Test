9c57b02348069399c2ada1e54a2694b497440e55
mseeger <mseeger@mseeger-ltm.sfdcgomobile.salesforce.com>
2017-06-14 13:17:34 -0500

    Added Products Controller

---
+++ controllers/products.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Default Controller
+ *
+ * @author mseeger
+ */
+class test extends Controller{
+    /**
+     *
+     * @var Salesforce 
+     */
+    
+    public function __construct() {
+        parent::__construct();
+        
+    }
+    
+    public function Index(){
+        $sfdc = new SFDCConnector();
+        $products = $sfdc->query("SELECT Name, Id, ProductCode, Description FROM Product2 WHERE Name != '' AND Description != '' AND ProductCode != '' ORDER BY Name ASC LIMIT 30");
+        $this->view->renderHeader();
+        
+        $this->view->render("productListing", $products);
+       
+        $this->view->renderFooter();
+    }
+}
+
+?>
