c33e6a11f6710fca04be5e9bb00015371be10574
mseeger <mseeger@mseeger-ltm.sfdcgomobile.salesforce.com>
2017-06-16 12:37:53 -0500

    Added View for Product Option Added Loop In feature View to Render Each Product Option

--- views/feature.php
+++ views/feature.php
@@ -1,3 +1,12 @@
-<div class='w3-container w3-bordered'>
-    <h3><?=$context->Name?></h3>    
+<div class='w3-container w3-border-red w3-round'>
+    <h3><?=$context->Name?></h3>
+    <p>Min Selections: <?=$context->MinOptions?></p>
+    <p>Max Selections: <?=$context->MaxOptions?></p>
+    <div class="w3-container">
+        <?php
+        foreach($context->options as $option){
+            $this->render('productOption',$option);
+        }
+        ?>
+    </div>
 </div>
