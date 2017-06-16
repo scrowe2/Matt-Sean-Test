ec2b7ced836265b48a2fc33f52ce76ab34295949
mseeger <mseeger@mseeger-ltm.sfdcgomobile.salesforce.com>
2017-06-16 12:28:06 -0500

    Added a View for each feture of a configuration. Added a loop in the config view to render each feature

--- views/configuration.php
+++ views/configuration.php
@@ -2,4 +2,8 @@
     <h2><?= $context->product->Name ?></h2>
     <h3><?= $context->product->SKU ?></h3>
     <p class="w3-text-grey"><?= $context->product->Description?></p>
+    <?php foreach($context->features as $feature){ 
+        $this->render('feature',$feature);
+    } 
+    ?>
 </div>
---
+++ views/feature.php
@@ -0,0 +1,3 @@
+<div class='w3-container w3-bordered'>
+    <h3><?=$context->Name?></h3>    
+</div>
