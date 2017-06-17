0600971b70d803044d3e796ccc0dd570b73ce2e9
mseeger <mseeger@mseeger-ltm.sfdcgomobile.salesforce.com>
2017-06-16 22:00:01 -0500

    Moved Summary Bar to the Top. Made Big Buttons.

--- views/configuration.php
+++ views/configuration.php
@@ -9,10 +9,10 @@
         ?>
     </div>
     <div class="w3-container w3-quarter">
-        <div class=" w3-container w3-bottom">
+        <div class=" w3-container w3-top" style="margin-top: 80px;">
             <h3>Configuration Summary</h3>
-            <a class="w3-button">Calculate</a>
-            <a class="w3-button">Get a Quote</a>
+            <div><a class="w3-button w3-red w3-hover-black w3-hover-text-red">Calculate</a></div>
+            <div><a class="w3-button w3-black w3-hover-red w3-hover-text-black">Get a Quote</a></div>
         </div>
     </div>
 </div>
