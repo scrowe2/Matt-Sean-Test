c6a358b9d14f1ae78daf4b5ec6553676eec9e338
mseeger <mseeger@mseeger-ltm.sfdcgomobile.salesforce.com>
2017-06-16 21:36:11 -0500

    Testing if a Summary Bar Looks OK. Trying to Force it to the bottom

--- views/configuration.php
+++ views/configuration.php
@@ -1,5 +1,5 @@
 <div class="w3-container">
-    <div class="w3-container w3-twothird"
+    <div class="w3-container w3-threequarter"
         <h2><?= $context->product->Name ?></h2>
         <h3><?= $context->product->SKU ?></h3>
         <p class="w3-text-grey"><?= $context->product->Description?></p>
@@ -8,14 +8,12 @@
         } 
         ?>
     </div>
-    <div class="w3-container w3-third">
-        <h3>Configuration Summary</h3>
-        <table>
-            <tr>
-                <td>Calculate</td>
-                <td>Get a Quote</td>
-            </tr>
-        </table>
+    <div class="w3-container w3-quarter">
+        <div class=" w3-container w3-bottom">
+            <h3>Configuration Summary</h3>
+            <a class="w3-button">Calculate</a>
+            <a class="w3-button">Get a Quote</a>
+        </div>
     </div>
 </div>
 <script>
