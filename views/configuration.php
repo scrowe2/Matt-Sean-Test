d5d5cc4e86461010eeca1862d6c0fb8f98225170
mseeger <mseeger@mseeger-ltm.sfdcgomobile.salesforce.com>
2017-06-16 21:27:02 -0500

    Trying a Config Page with a summary

--- views/configuration.php
+++ views/configuration.php
@@ -1,12 +1,24 @@
 <div class="w3-container">
-    <h2><?= $context->product->Name ?></h2>
-    <h3><?= $context->product->SKU ?></h3>
-    <p class="w3-text-grey"><?= $context->product->Description?></p>
-    <?php foreach($context->features as $feature){ 
-        $this->render('feature',$feature);
-    } 
-    ?>
-    <script>
-        productModel = <?=$context->LoadFromIdJSON ?>;
-    </script>
+    <div class="w3-container w3-twothird"
+        <h2><?= $context->product->Name ?></h2>
+        <h3><?= $context->product->SKU ?></h3>
+        <p class="w3-text-grey"><?= $context->product->Description?></p>
+        <?php foreach($context->features as $feature){ 
+            $this->render('feature',$feature);
+        } 
+        ?>
+    </div>
+    <div class="w3-container w3-third">
+        <h3>Configuration Summary</h3>
+        <table>
+            <tr>
+                <td>Calculate</td>
+                <td>Get a Quote</td>
+            </tr>
+        </table>
+    </div>
 </div>
+<script>
+    productModel = <?=$context->LoadFromIdJSON ?>;
+</script>
+
