767a8b27750ec70e5dd5d43c4e9a8a06174a4323
mseeger <mseeger@mseeger-ltm.sfdcgomobile.salesforce.com>
2017-06-25 10:26:06 -0500

    Fixed broken Div Tag

--- views/configuration.php
+++ views/configuration.php
@@ -1,5 +1,5 @@
 <div class="w3-container">
-    <div class="w3-container w3-threequarter"
+    <div class="w3-container w3-threequarter">
         <h2><?= $context->product->Name ?></h2>
         <h3><?= $context->product->SKU ?></h3>
         <p class="w3-text-grey"><?= $context->product->Description?></p>
