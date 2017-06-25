2fe122011a9d92d675994c3ffcbe0bf74afc0ed2
mseeger <mseeger@mseeger-ltm.sfdcgomobile.salesforce.com>
2017-06-25 11:38:11 -0500

    Added the form to the configuration page

--- views/configuration.php
+++ views/configuration.php
@@ -1,20 +1,22 @@
 <div class="w3-container">
-    <div class="w3-container w3-threequarter">
-        <h2><?= $context->product->Name ?></h2>
-        <h3><?= $context->product->SKU ?></h3>
-        <p class="w3-text-grey"><?= $context->product->Description?></p>
-        <?php foreach($context->features as $feature){ 
-            $this->render('feature',$feature);
-        } 
-        ?>
-    </div>
-    <div class="w3-container w3-quarter">
-        <div class=" w3-container w3-top" style="margin-top: 80px;">
-            <h3>Configuration Summary</h3>
-            <div><a class="w3-button w3-red w3-hover-black w3-hover-text-red">Calculate</a></div>
-            <div><a class="w3-button w3-black w3-hover-red w3-hover-text-black">Get a Quote</a></div>
+    <form name="configForm">
+        <div class="w3-container w3-threequarter">
+            <h2><?= $context->product->Name ?></h2>
+            <h3><?= $context->product->SKU ?></h3>
+            <p class="w3-text-grey"><?= $context->product->Description?></p>
+            <?php foreach($context->features as $feature){ 
+                $this->render('feature',$feature);
+            } 
+            ?>
         </div>
-    </div>
+        <div class="w3-container w3-quarter">
+            <div class=" w3-container w3-top" style="margin-top: 80px;">
+                <h3>Configuration Summary</h3>
+                <div><a class="w3-button w3-red w3-hover-black w3-hover-text-red">Calculate</a></div>
+                <div><a class="w3-button w3-black w3-hover-red w3-hover-text-black">Get a Quote</a></div>
+            </div>
+        </div>
+    </form>
 </div>
 <script>
     productModel = <?=$context->LoadFromIdJSON ?>;
