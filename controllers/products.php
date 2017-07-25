396901c09b06dbd98eadc7d33dde5ad161896526
scrowesalesforce <scrowe@salesforce.com>
2017-07-25 13:31:45 -0500

    Update products.php

--- controllers/products.php
+++ controllers/products.php
@@ -16,7 +16,12 @@ class products extends Controller{
     }
     
     public function Index(){
-        //$sfdc = new SFDCConnector();
+        try{
+            $sfdc = new SFDCConnector();
+        }
+        catch(Exceptions $e){
+            var_dump($e);
+        }
         //$products = $sfdc->query("SELECT Name, SBQQ__ProductPictureID__c, Id, ProductCode, Description, Image__c, WebConfigurable__c FROM Product2 WHERE AvailableOnWeb__c = TRUE AND Name != '' AND Description != '' AND ProductCode != '' ORDER BY WebConfigurable__c DESC, Name ASC LIMIT 30");
         $this->view->renderHeader();
        // $this->view->render("productListing", $products);
