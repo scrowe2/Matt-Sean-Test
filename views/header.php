1a43978f79981fda2807bc4e3163e1b984650413
scrowesalesforce <scrowe@salesforce.com>
2017-07-26 11:46:50 -0500

    Update header.php

--- views/header.php
+++ views/header.php
@@ -4,7 +4,7 @@
 <title>RHINO Manufacturing</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="/public/css/w3.css">
+<link rel="stylesheet" href="/public/css/w3.php">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
