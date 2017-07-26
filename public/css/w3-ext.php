93a5ae20092d8f9acee4f37d444c065cf9fce391
scrowesalesforce <scrowe@salesforce.com>
2017-07-26 12:00:33 -0500

    Update and rename w3-ext.css to w3-ext.php

--- public/css/w3-ext.php
+++ public/css/w3-ext.php
@@ -1,3 +1,4 @@
+<?php header("Content-type: text/css"); ?>
 body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
 .w3-bar,h1,button {font-family: "Montserrat", sans-serif}
 .fa-big-image {font-size:200px}
