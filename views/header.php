1c0e55dea7e65c34057e1f761c6105b0107847ac
scrowesalesforce <scrowe@salesforce.com>
2017-07-27 09:17:54 -0500

    Update header.php

--- views/header.php
+++ views/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>RHINO Manufacturing</title>
+<title><?=CNAME?></title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="/public/css/w3.php?theme=<?=PRIM?>">
